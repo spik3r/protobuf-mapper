e7d9f2cc89cd412b663e11cbf8a6e9e0cd530854
Kai <kai-tait@macolighting.com>
2017-03-20 15:39:48 +1000

    cleaning up

--- src/main/java/Mapper.java
+++ src/main/java/Mapper.java
@@ -7,18 +7,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
-import sun.misc.Unsafe;
-
-/**
- * Created by kai-tait on 20/03/2017.
- */
 public class Mapper {
     private static HashMap hmap;
     
     public static void main(String[] args) throws Exception {
-//        Foo foo = new Foo();
-//        getMemberFields(foo);
-//        DummyBar dummyBar = (DummyBar) createClass(DummyBar.class, 99.99, "barbarbar");
         System.out.println("_____createClass______");
         Bar bar = new Bar(98765.4321, "foobar");
         System.out.println("Original class: " + bar.getClass());
@@ -30,47 +22,7 @@ public class Mapper {
         
         System.out.println("_____after createClass______");
     }
-    
-    public static void getMemberFields(Object obj) throws IllegalAccessException, NoSuchFieldException {
-        Class<?> objClass = obj.getClass();
-        System.out.println("obj: " + obj.getClass());
-        
-        Field[] fields = objClass.getDeclaredFields();
-        for(Field field : fields) {
-            field.setAccessible(true);
-            System.out.println("Field: " + field.getName() + " value: " + field.get(obj));
-            
-            Field[] innerFields = {};
-            if (!field.getType().getName().contains("java.lang")){
-                innerFields = field.getType().getDeclaredFields();
-            }
-            
-            for (Field innerField: innerFields) {
-                if (!innerField.getName().contains("java.lang")) {
-                    Object subObj = field.getType();
-                    
-                    innerField.setAccessible(true);
-                    System.out.println("");
-                    
-                    System.out.println("Sub Field: " + innerField.getName());
-                    //Todo: replace new Bar() with object
-                    System.out.println( "Sub Field value: " + innerField.get(new Bar()));
-                }
-                
-            }
-        }
-    }
-    
-    public static Object createClass(Class clazz, Double someDouble, String someString) throws
-            IllegalAccessException,
-            InstantiationException, NoSuchMethodException, InvocationTargetException
-    {
-        Object someClass = clazz.asSubclass(clazz).getConstructor(Double.class, String.class)
-            .newInstance(someDouble, someString);
 
-         return someClass;
-    }
-    
     
     private static HashMap<String, Object> getMemberFieldss(Object obj) throws IllegalAccessException,
             NoSuchFieldException
@@ -85,7 +37,7 @@ public class Mapper {
             field.setAccessible(true);
             System.out.println("Field: " + field.getName() + " value: " + field.get(obj));
             fieldValues.put(field.getName(), field.get(obj));
-            // Instead you'd use the commons class to check if it is primitive or an object
+            // Todo: use the commons class to check if it is primitive or an object
             if (!field.getType().isPrimitive() && !field.getType().getName().contains("java.lang"))
             {
                 getMemberFieldss(field.get(obj));
@@ -124,12 +76,8 @@ public class Mapper {
         System.out.println("values: " + Arrays.toString(values));
         
         Constructor constructor = clazz.getConstructor(classes);
-//        Object someClass = clazz.asSubclass(clazz).getConstructor(classes)
         constructor.setAccessible(true);
         Object someClass = constructor.newInstance(values);
-
-//        Object someClass = clazz.newInstance();
-//        Object someClass = Class.forName(clazz.getName());
         
         System.out.println("someCLass: " + someClass.getClass());
         setMemberFields(someClass);
@@ -148,9 +96,7 @@ public class Mapper {
         {
             field.setAccessible(true);
             Object fieldValue = hmap.get(field.getName());
-            if (fieldValue == null) {
-                fieldValue = DefaultValues.generateValue(field.getType());
-            }
+
             field.set(obj, fieldValue);
         }
     }
